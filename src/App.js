c0c8b10881b62117075b916b9f72fe9702f21e7c
Martin <martinlynsa@gmail.com>
2022-12-21 14:33:27 -0300

    correcciones

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import ColorSchemesExample from "./Components/NavBar.js";
-import CartWidget from "./Components/CartWidget/CartWidget.js"
+import ColorSchemesExample from "./Components/NavBar/NavBar.jsx";
+import { ItemListContainer } from "./Components/ItemListContainer/ItemListContainer.jsx";
 
 const App = () => {
-
-
-
-
-
   return (
-   <>
-   
+    
    <div>
       <ColorSchemesExample />
+      <ItemListContainer title="Bienvenido a Gaia Games Store, la mejor LudoTeca"/>
     </div>
-    <div>
-      <CartWidget />
-    </div>
-   
-   </>
   );
 }
 
